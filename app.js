24386d83db4e8b4faaa3dfc87860a8c20f2a5f7c
Stephen Schroeder <schrode50@gmail.com>
2017-11-01 02:55:08 -0700

    getting twitter push going

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 function getQuote() {
   $('.new_quote').on('click', function(e) {
     e.preventDefault();
+    var random = Math.floor(Math.random() * 40);
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&callback=", function(a) {
       // console.log(a);
-      var random = Math.floor(Math.random() * 40);
       $('.new_text').empty();
       $(".new_text").append(a[random].content + "<p>— " + a[random].title + "</p>")
-      // console.log('success');
+            // console.log('success');
+
+      $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?&text=' + encodeURIComponent('"' + a[random].content + '" ' + "-" + a[random].title));
     })
   });
 }
